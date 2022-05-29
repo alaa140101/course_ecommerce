143aee11cf197ba6f4b58f864b8e9cf76042fc25
alaa140101 <alaa140011@gmail.com>
2022-05-29 06:06:04 +0300

    end autentications

--- routes/web.php
+++ routes/web.php
@@ -12,13 +12,22 @@ Route::get('/singleProduct', [FrontendController::class,'singleProduct'])->name(
 Route::get('/checkout', [FrontendController::class,'checkout'])->name('frontend.checkout');
 Route::get('/contact', [FrontendController::class,'contact'])->name('frontend.contact');
 
-
-Route::get('/admin/login', [BackendController::class,'login'])->name('backend.login');
-Route::get('/admin/index', [BackendController::class,'index'])->name('backend.index');
-Route::get('/admin/blank', [BackendController::class,'blank'])->name('backend.blank');
-Route::get('/admin/forgot-password', [BackendController::class,'forgot_password'])->name('backend.forgot-password');
-
-
 Auth::routes(['verify' => true]);
+
+Route::group(['prefix'=>'admin', 'as'=>'admin.'], function(){
+  Route::group(['middleware'=>'guest'], function(){
+    Route::get('/login', [BackendController::class,'login'])->name('login');
+    Route::get('/blank', [BackendController::class,'blank'])->name('blank');
+    Route::get('/forgot-password', [BackendController::class,'forgot_password'])->name('forgot-password');
+  });
+  
+  Route::group(['middleware'=> ['roles', 'role:admin|supervisor']], function(){
+    Route::get('/', [BackendController::class,'index'])->name('index_route');
+    Route::get('/index', [BackendController::class,'index'])->name('index');
+  });
+
+});
+
+
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
